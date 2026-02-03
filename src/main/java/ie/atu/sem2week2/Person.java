174ae7fae8d5caf2ac9c372af81f3765e8590b1c
deira <deiravalci@gmail.com>
2026-02-03 22:48:36 +0000

    Added more endpoints

---
+++ src/main/java/ie/atu/sem2week2/Person.java
@@ -0,0 +1,33 @@
+package ie.atu.sem2week2;
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person()
+    {
+        this.name = "";
+        this.age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/ie/atu/sem2week2/RequestController.java
+++ src/main/java/ie/atu/sem2week2/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.sem2week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,18 @@ public class RequestController {
     public String getByName(@PathVariable String name)
     {
         return "Hello " + name;
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam Integer age)
+    {
+        return "Hello " + name + " " + age;
+    }
+
+    @GetMapping("/person")
+    public Person getPerson()
+    {
+        Person person = new Person("Deira", 24);
+        return person;
     }
 }
