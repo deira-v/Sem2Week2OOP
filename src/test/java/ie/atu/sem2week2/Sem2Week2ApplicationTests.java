977ed85b5c8409cdb9b118e3ee8d047dbff1607e
deira <deiravalci@gmail.com>
2026-02-03 21:59:32 +0000

    Created Project

---
+++ src/test/java/ie/atu/sem2week2/Sem2Week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.sem2week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2Week2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
