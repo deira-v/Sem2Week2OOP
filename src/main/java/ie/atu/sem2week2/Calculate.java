3d3e4bc95de876ef4ea351a2fa055fd8688cee25
deira <deiravalci@gmail.com>
2026-03-17 09:02:11 +0000

    Fixed error handler

--- src/main/java/ie/atu/sem2week2/Calculate.java
+++ src/main/java/ie/atu/sem2week2/Calculate.java
@@ -1,17 +1,22 @@
 package ie.atu.sem2week2;
 
+import lombok.Setter;
+
 public class Calculate {
     private int a;
     private int b;
+    private String operation;
 
-    public Calculate(int a, int b){
+    public Calculate(int a, int b, String operation){
         this.a = a;
         this.b = b;
+        this.operation = operation;
     }
 
     public Calculate(){
         this.a = 0;
         this.b = 0;
+        this.operation = "";
     }
 
     public int getNum1() {
@@ -31,8 +36,22 @@ public class Calculate {
     }
 
     //Addition
-    public static int add(Integer a, Integer b)
-    {
+    public static int add(int a, int b) {
         return a + b;
+    }
+
+    //subtraction
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    //Multiplication
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    //Division
+    public static float divide(int a, int b) {
+        return (float) a / b;
     }
 }
--- src/main/java/ie/atu/sem2week2/RequestController.java
+++ src/main/java/ie/atu/sem2week2/RequestController.java
@@ -8,56 +8,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World";
     }
 
     @GetMapping("/greet/{name}")
-    public String getByName(@PathVariable String name)
-    {
+    public String getByName(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 
     @GetMapping("/details")
-    public String details(@RequestParam String name, @RequestParam Integer age)
-    {
+    public String details(@RequestParam String name, @RequestParam Integer age) {
         return "Hello " + name + ", Age: " + age;
     }
 
     @GetMapping("/person")
-    public Person getPerson()
-    {
+    public Person getPerson() {
         Person person = new Person("Deira", 24);
         return person;
     }
 
-    @GetMapping("/add")
-    public int add(@RequestParam int a, @RequestParam int b){
-        Calculate calculate = new Calculate();
-        int result = Calculate.add(a, b);
-        return result;
+    @GetMapping("/calculate")
+    public float calculate(@RequestParam int a, @RequestParam int b, @RequestParam String operation) {
+
+        return switch (operation.toLowerCase()) {
+            case "add" -> Calculate.add(a, b);
+            case "subtract" -> Calculate.subtract(a, b);
+            case "multiply" -> Calculate.multiply(a, b);
+            case "divide" -> {
+                if (b == 0) {
+                    throw new IllegalArgumentException("You can't divide by zero!");
+                }
+                yield Calculate.divide(a, b);
+            }
+            default -> throw new IllegalArgumentException("Invalid operation!");
+        };
     }
-
-//    @GetMapping("/calculate/{operation}")
-//    public String getOperation(@PathVariable String operation)
-//    {
-//        Calculate calculate = new Calculate(3, 4);
-//        return Calculate.add();
-//    }
-
-
 }
-
-/*
-switch(operate)
-        {
-        case "add" ->
-public Calculate add(@RequestParam int num1, @RequestParam int num2)
-{
-    Calculate calc = new Calculate();
-    double result = calc.add(num1, num2);
-    return result;
-}
-        }
-
-*/
